84867fb32bd9ec96e7a6a841c3a7a4f2484fe62f
Joni Weiss <joni@handmadepixels.com>
2016-10-08 14:48:51 -0600

    feat(core) Add layout, styles; Add JS code.
    Most of the JS code is added in this commit.
    Still need JS for evaluating character received,
    comparing with word[0], and all else pertaining to
    game functionality.

--- js/app.js
+++ js/app.js
@@ -1,22 +1,73 @@
 "use strict";
 
-var min = 0;
-var max = 99;
-var randomNum;
-var nextWord;
+const min = 0;
+const max = 99;
+let randomNum;
+let nextWord = [];
+let nextWordName;
+let nextWordLength;
+let numTries = 0;
+let numTriesLeft = 0;
+let currChar = '';
 
 function getRandNum (imin,imax) {
-  var floatNum = Math.random() * (imax - imin) + imin;
+  let floatNum = Math.random() * (imax - imin) + imin;
   return Math.floor(floatNum);
 }
 
-randomNum = getRandNum(min,max);
-
-nextWord = commonWords[randomNum];
-
-function displayMsg() {
-  console.log(randomNum);
-  console.log(nextWord);
+function getNextWord (randomNum) {
+  nextWordName = commonWords[randomNum];
+  nextWordLength = nextWordName.length;
+  return [nextWordName,nextWordLength];
 }
 
-window.onload = displayMsg;
+function displayMsg() {
+  // console.log("DisplayMsg");
+  let msg = "DisplayMsg \n" + min + " & " + max;
+  document.querySelector('h2#guess').innerHTML = msg;
+  document.getElementById('input-guess').focus();
+}
+
+function clearGame() {
+  console.log("clearGame");
+  numTries = 0;
+  numTriesLeft = 8;
+}
+
+function playGame (word) {
+  console.log("playGame");
+  let msgHeading = '';
+  let msgOutput = '';
+  let numSpaces = word[1];
+  let spaceWord = '';
+  for (let i = 0; i < numSpaces; i++) {
+    spaceWord += '<li></li>';
+  }
+  msgHeading = word[0] + "\n";
+  msgOutput = spaceWord + "\n";
+  console.log(word + " - " + spaceWord);
+  document.querySelector('h2#dispOutput').innerHTML = msgHeading;
+  document.querySelector('div#wordOutput').innerHTML = msgOutput;
+}
+
+function getChar()
+{
+  let currChar = document.getElementById('input-guess');
+  console.log("Got " + currChar.value);
+  let div = document.getElementById('wordOutput');
+  div.innerHTML = div.innerHTML + currChar.value;
+  document.querySelector('input').value = '';
+  return currChar;
+}
+
+function onPageLoad() {
+  clearGame();
+  displayMsg();
+  randomNum = getRandNum(min,max);
+  nextWord = getNextWord(randomNum);
+  playGame(nextWord);
+  // console.log(randomNum + "," + nextWord);
+}
+
+// document.querySelector('button.btn-guess').onkeypress = getChar;
+window.onload = onPageLoad;
