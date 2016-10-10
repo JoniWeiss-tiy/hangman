c253d89ab3463e3b5a58372ac24130aa1e57d715
Joni Weiss <joni@handmadepixels.com>
2016-10-09 21:29:15 -0600

    feat(core) UI cosmetic changes; Disallow repeats of same char

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@
 const min = 0;
 const max = 99;
 const maxTries = 8;
+let lettersTried = '';
 let numTriesLeft = maxTries;
 let currWord = [];
 let matchWordArr = [];
@@ -69,61 +70,67 @@ function getChar() {
     let currChar =  document.getElementById('input-guess').value.toLowerCase();
     // Check char for valid input;
     if (alphaBet.indexOf(currChar) !== -1) {
-      // Show user all previous valid choices
-      let guessed = document.getElementById('guessed');
-      guessed.innerHTML = guessed.innerHTML + currChar;
-      // decrement number left.
-      numTriesLeft = numTriesLeft - 1;
-      let turnsLeft = document.getElementById('turns-left');
-      turnsLeft.innerHTML = turnsLeft.innerHTML = numTriesLeft;
-      // Update matchWordArr with correct guessed letters
-      for (let i = 0; i <currWordArr.length; i++) {
-          if (currWordArr[i] === currChar) {
-            matchWordArr[i] = currChar;
-          }
-      }
+      // Don't allow a letter already been used.
+      if (lettersTried.indexOf(currChar) === -1) {
+        // Show user all previous valid choices
+        lettersTried += currChar;
+        let guessed = document.getElementById('guessed');
+        guessed.innerHTML = guessed.innerHTML = lettersTried;
+        // decrement number left.
+        numTriesLeft = numTriesLeft - 1;
+        let turnsLeft = document.getElementById('turns-left');
+        turnsLeft.innerHTML = turnsLeft.innerHTML = numTriesLeft;
+        // Update matchWordArr with correct guessed letters
+        for (let i = 0; i <currWordArr.length; i++) {
+            if (currWordArr[i] === currChar) {
+              matchWordArr[i] = currChar;
+            }
+        }
 
-      // Loop through matchWordArr and re-build html for
-      // output area, including correctly-guessed letters
-      spaceWordArr.length = 0; // empty array
-      for (var i = 0 ; i < matchWordArr.length ; i++ ) {
-        if (matchWordArr[i] !== undefined ) {
-          for (let i = 0; i < currWordArr.length; i++) {
-              if (currWordArr[i] === matchWordArr[i]) {
+        // Loop through matchWordArr and re-build html for
+        // output area, including correctly-guessed letters
+        spaceWordArr.length = 0; // empty array
+        for (var i = 0 ; i < matchWordArr.length ; i++ ) {
+          if (matchWordArr[i] !== undefined ) {
+            for (let i = 0; i < currWordArr.length; i++) {
+                if (currWordArr[i] === matchWordArr[i]) {
+                  // getting undefined unless initialize to '' before using.
+                  spaceWordArr[i] = '';
+                  spaceWordArr[i] += '<li><p>' + currWordArr[i] + '</p></li>';
+                } else {
+                  // getting undefined unless initialize to '' before using.
+                  spaceWordArr[i] = '';
+                  spaceWordArr[i] += '<li><p></p></li>'
+                }
+            }
+            spaceWord = spaceWordArr.join('');
+            matchWord = matchWordArr.join('');
+
+            document.querySelector('div#wordOutput').innerHTML = spaceWord;
+
+            function disableInput() {
+              document.getElementById("input-guess").disabled = true;
+            }
+
+            if (matchWord === currWord[0] ) {
+              displayMsg("win");
+              disableInput();
+            } else if (numTriesLeft === 0) {
+              displayMsg("lose");
+              disableInput();
+              spaceWordArr.length = 0; // empty array
+              for (let i = 0; i < currWordArr.length; i++) {
                 // getting undefined unless initialize to '' before using.
                 spaceWordArr[i] = '';
                 spaceWordArr[i] += '<li><p>' + currWordArr[i] + '</p></li>';
-              } else {
-                // getting undefined unless initialize to '' before using.
-                spaceWordArr[i] = '';
-                spaceWordArr[i] += '<li><p></p></li>'
               }
-          }
-          spaceWord = spaceWordArr.join('');
-          matchWord = matchWordArr.join('');
-
-          document.querySelector('div#wordOutput').innerHTML = spaceWord;
-
-          function disableInput() {
-            document.getElementById("input-guess").disabled = true;
-          }
-
-          if (matchWord === currWord[0] ) {
-            displayMsg("win");
-            disableInput();
-          } else if (numTriesLeft === 0) {
-            displayMsg("lose");
-            disableInput();
-            spaceWordArr.length = 0; // empty array
-            for (let i = 0; i < currWordArr.length; i++) {
-              // getting undefined unless initialize to '' before using.
-              spaceWordArr[i] = '';
-              spaceWordArr[i] += '<li><p>' + currWordArr[i] + '</p></li>';
+              spaceWord = spaceWordArr.join('');
+              document.querySelector('div#wordOutput').innerHTML = spaceWord;
             }
-            spaceWord = spaceWordArr.join('');
-            document.querySelector('div#wordOutput').innerHTML = spaceWord;
           }
         }
+      } else {
+        alert("You have tried " + currChar + " before! Remember? Try again...");
       }
     } else {
       alert(currChar + " is not a valid character! Try again...");
